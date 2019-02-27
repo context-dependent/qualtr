d21550983ba57a2c1fe0c9cb48a52da99983ee43
context-dendent <tmcmanus@blueprint-ade.ca>
2019-02-27 13:18:41 -0500

    readme.rmd

--- R/06_scr-num.R
+++ R/06_scr-num.R
@@ -45,6 +45,74 @@ scc <- function() {
 
 }
 
+likert_scales <- list(
+  agree = c(
+    "Strongly disagree"                  = 1,
+    "Disagree"                           = 2,
+    "Neutral"                            = 3,
+    "Agree"                              = 4,
+    "Strongly agree"                     = 5
+  ),
+  confident = c(
+    "Not at all confident"               = 1,
+    "A little confident"                 = 2,
+    "Somewhat confident"                 = 3,
+    "Very confident"                     = 4,
+    "Completely confident"               = 5
+  ),
+  difficult = c(
+    "Extreme difficulty or can't do"     = 1,
+    "A lot of difficulty"                = 2,
+    "Some difficulty"                    = 3,
+    "A little difficulty"                = 4,
+    "No difficulty"                      = 5
+  ),
+  often = c(
+    "Never"                              = 1,
+    "Once or twice"                      = 2,
+    "About once a week"                  = 3,
+    "About 2 or 3 times a week"          = 4,
+    "Almost every day"                   = 5,
+    "Every day"                          = 6
+  ),
+  often_2 = c(
+    "Almost never"                       = 1,
+    "Rarely"                             = 2,
+    "Sometimes"                          = 3,
+    "Often"                              = 4,
+    "Most of the time"                   = 5
+  ),
+  often_3 = c(
+    "Never"                              = 1,
+    "Rarely"                             = 2,
+    "Sometimes"                          = 3,
+    "Often"                              = 4,
+    "All of the time"                    = 5
+  ),
+  effect_size = c(
+    "No effect"                          = 1,
+    "Small effect"                       = 2,
+    "Medium effect"                      = 3,
+    "Large effect"                       = 4,
+    "Very large effect"                  = 5
+  ),
+  satisfied = c(
+    "Extremely dissatisfied"             = 1,
+    "Somewhat dissatisfied"              = 2,
+    "Neither satisfied nor dissatisfied" = 3,
+    "Somewhat satisfied"                 = 4,
+    "Extremely satisfied"                = 5
+  ),
+  interested = c(
+    "Not interested at all"              = 1,
+    "Slightly interested"                = 2,
+    "Somewhat interested"                = 3,
+    "Very interested"                    = 4,
+    "Extremely interested"               = 5
+
+  )
+)
+
 #' Recode scale with numeric values
 #'
 #' @param dat
@@ -59,84 +127,19 @@ scc <- function() {
 #' @examples
 scr_num <- function(dat, .vars, .rev = NULL, scale = "agree", fct = FALSE) {
 
-  scales <- list(
-    agree = c(
-      "Strongly disagree"                  = 1,
-      "Disagree"                           = 2,
-      "Neutral"                            = 3,
-      "Agree"                              = 4,
-      "Strongly agree"                     = 5
-    ),
-    confident = c(
-      "Not at all confident"               = 1,
-      "A little confident"                 = 2,
-      "Somewhat confident"                 = 3,
-      "Very confident"                     = 4,
-      "Completely confident"               = 5
-    ),
-    difficult = c(
-      "Extreme difficulty or can't do"     = 1,
-      "A lot of difficulty"                = 2,
-      "Some difficulty"                    = 3,
-      "A little difficulty"                = 4,
-      "No difficulty"                      = 5
-    ),
-    often = c(
-      "Never"                              = 1,
-      "Once or twice"                      = 2,
-      "About once a week"                  = 3,
-      "About 2 or 3 times a week"          = 4,
-      "Almost every day"                   = 5,
-      "Every day"                          = 6
-    ),
-    often_2 = c(
-      "Almost never"                       = 1,
-      "Rarely"                             = 2,
-      "Sometimes"                          = 3,
-      "Often"                              = 4,
-      "Most of the time"                   = 5
-    ),
-    often_3 = c(
-      "Never"                              = 1,
-      "Rarely"                             = 2,
-      "Sometimes"                          = 3,
-      "Often"                              = 4,
-      "All of the time"                    = 5
-    ),
-    effect_size = c(
-      "No effect"                          = 1,
-      "Small effect"                       = 2,
-      "Medium effect"                      = 3,
-      "Large effect"                       = 4,
-      "Very large effect"                  = 5
-    ),
-    satisfied = c(
-      "Extremely dissatisfied"             = 1,
-      "Somewhat dissatisfied"              = 2,
-      "Neither satisfied nor dissatisfied" = 3,
-      "Somewhat satisfied"                 = 4,
-      "Extremely satisfied"                = 5
-    ),
-    interested = c(
-      "Not interested at all"              = 1,
-      "Slightly interested"                = 2,
-      "Somewhat interested"                = 3,
-      "Very interested"                    = 4,
-      "Extremely interested"               = 5
 
-    )
-  )
 
-  cludge <- mean(scales[[scale]])
+  cludge <- mean(likert_scales[[scale]])
 
   dat <-
+
     dat %>%
       dplyr::mutate_at(
         .vars,
         dplyr::funs(
           . %>%
             trimws() %>%
-            purrr::map_dbl(~scales[[scale]][.x]))
+            purrr::map_dbl(~likert_scales[[scale]][.x]))
         )
 
   if(!is.null(.rev)) {
